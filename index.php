6f763088428831102103da5f897f12fb28a3e2d9
yshiga <yuichi.shiga@gmail.com>
2017-02-26 15:12:09 +0900

    得点を表示

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-require_once './vendor/spyc/Spyc.php';
-$questions = Spyc::YAMLLoad('./questions/question0.yml');
-$questionTemplate = file_get_contents('./html/question.html');
-var_dump($_POST);
-?>
 <html lang="ja" >
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,14 +8,36 @@ var_dump($_POST);
 <body ng-app="MyApp" ng-cloak>
 <section layout="row" layout-sm="column" layout-align="center center" layout-wrap>
 	<div ng-controller="AppCtrl">
-	  <form ng-submit="submit()" ng-cloak>
-	<?php foreach($questions as $i => $question): ?>
-	<?php echo $html = strtr($questionTemplate, array('^i' => $i + 1, '^title' => $question['title'])); ?>
-	<?php endforeach; ?>
+	<form name="questionForm" ng-submit="submit()" ng-cloak novalidate>
+<div ng-if="questionForm.$submitted">
+<p>あなたの得点は{{ score }} 点です</p>
+</div>
+<div ng-repeat="(key, question) in questions">
+<p>[問題{{key + 1}}] {{ question.title }} </p>
+<md-radio-group ng-model='question.value' required>
+<md-radio-button name="i-1" value="1" class="md-primary">はい</md-radio-button>
+<md-radio-button name="i-0" value="0">いいえ</md-radio-button>
+<div ng-if="questionForm.$submitted">
+	<div ng-if="question.value == question.answer">
+		<span>正解</span>
+	</div>
+	<div ng-if="question.value != question.answer">
+		<span>不正解</span>
+	</div>
+	<span>{{ question.explanation }}</span>
+</div>
+</md-radio-group>
+<hr />
+</div>
 
-    <button class="md-raised md-primary md-button md-ink-ripple" type="submit" ><span class="ng-scope">答えを見る</span><div class="md-ripple-container"></div></button>
-  </form>
+<p ng-show="questionForm.$invalid">
+全て回答してください。
+</p>
 
+	<button class="md-raised md-primary md-button md-ink-ripple" ng-disabled="questionForm.$invalid" type="submit" >
+		<span class="ng-scope">答えを見る</span>
+		<div class="md-ripple-container"></div></button>
+	</form>
 </div>
 </section>
 <!-- Angular Material requires Angular.js Libraries -->
@@ -35,12 +51,18 @@ var_dump($_POST);
   
 <!-- Your application bootstrap  -->
 <script type="text/javascript">    
-	angular.module('MyApp', ['ngMaterial']).controller('AppCtrl', function($scope) {
+	angular.module('MyApp', ['ngMaterial']).controller('AppCtrl', function($scope, $http) {
 		$scope.submit = function() {
-			alert('test' + $scope.data.group1);
+
+			var correctQuestions = $scope.questions.filter(function(val) {
+				return (val.value == val.answer);
+			});
+			$scope.score = correctQuestions.length / $scope.questions.length * 100;
 		}
 		$scope.data = {};
-
+		$http.get("./api/").then(function(response) {
+			$scope.questions = response.data;
+		});
 
 	}).config(function($mdIconProvider) {
 
