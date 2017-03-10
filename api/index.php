9f5826a6b2a2dc813291fefb668ef29cb5eb6aa7
yshiga <yuichi.shiga@gmail.com>
2017-03-10 16:57:42 +0900

    update

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once './vendor/spyc/Spyc.php';
-$questions = Spyc::YAMLLoad('./questions/question0.yml'); 
+$questions = Spyc::YAMLLoad('./questions/question1.yml'); 
 
 header('content-type: application/json; charset=utf-8');
 echo json_encode($questions);
